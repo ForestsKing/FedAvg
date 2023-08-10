0d7eeb262ddeefbc695c837fe673465f3345e4c5
ForestsKing <chengsen_wang@163.com>
2023-08-10 10:55:03 +0800

    fed_avg

--- script/run.sh
+++ script/run.sh
@@ -1,6 +1,6 @@
 for B in 16 64 256 1024; do
-  for E in 1 5 10; do
-    for C in 0.0 0.2 0.4 0.6 0.8 1.0; do
+  for E in 1 5 20 50; do
+    for C in 0.0 0.1 0.2 0.5 1.0; do
       python -u main.py \
         --batch_size $B \
         --local_epoch $E \
